032a2756b55b0f8c81937f17828697d356203f17
kunal fushiguru <natwarlaluzumaki@gmail.com>
2024-10-10 20:44:12 +0530

    feat: Added something

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,24 @@
-console.log("Server");
+// import app from "./app"
+// import { PORT, SERVER_URL } from "./config/config"
+
+// const server = app.listen(PORT)
+
+// ;(function () {
+//     try {
+//         console.info("Server Started ", {
+//             meta: {
+//                 port: PORT,
+//                 url: SERVER_URL
+//             }
+//         })
+//     } catch (error) {
+//         console.error(error)
+//         server.close((err) => {
+//             if (err) {
+//                 console.error(err.message)
+//             }
+//             process.exit(1)
+//         })
+//     }
+// })()
+
